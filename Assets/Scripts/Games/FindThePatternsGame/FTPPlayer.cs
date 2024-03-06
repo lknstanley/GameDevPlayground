a6c33a4adafe4f0529c721d111ec3933160e4b57
Stanley Lai <stanley.lai.104@gmail.com>
2024-03-06 21:35:27 +0800

    Added walking logic and effects that turns the road to green

--- Assets/Scripts/Games/FindThePatternsGame/FTPPlayer.cs
+++ Assets/Scripts/Games/FindThePatternsGame/FTPPlayer.cs
@@ -7,27 +7,80 @@ namespace Games.FindThePatternsGame
 {
     public class FTPPlayer : MonoBehaviour
     {
+        [ SerializeField ]
         private bool _canMove = true;
 
-        private void Update()
+        [ SerializeField ]
+        private Vector3 currentLocation;
+
+        private void FixedUpdate()
         {
-            if ( _canMove && Input.GetKey( KeyCode.UpArrow ) ) Move( Vector3.forward );
-            if ( _canMove && Input.GetKey( KeyCode.DownArrow ) ) Move( -Vector3.forward );
-            if ( _canMove && Input.GetKey( KeyCode.LeftArrow ) ) Move( Vector3.left );
-            if ( _canMove && Input.GetKey( KeyCode.RightArrow ) ) Move( Vector3.right );
+            if ( _canMove )
+            {
+                // Move up
+                if ( Input.GetKey( KeyCode.UpArrow ) && CanMoveTo( Vector3.forward ) )
+                    Move( Vector3.forward );
+                // Move down
+                if ( Input.GetKey( KeyCode.DownArrow ) && CanMoveTo( -Vector3.forward ) )
+                    Move( -Vector3.forward );
+                // Move left
+                if ( Input.GetKey( KeyCode.LeftArrow ) && CanMoveTo( Vector3.left ) )
+                    Move( Vector3.left );
+                // Move right
+                if ( Input.GetKey( KeyCode.RightArrow ) && CanMoveTo( Vector3.right ) )
+                    Move( Vector3.right );
+            }
         }
 
+        /// <summary>
+        ///     Move the player to the given direction
+        /// </summary>
+        /// <param name="dir"></param>
         private void Move( Vector3 dir )
         {
+            SetCurrentLocation( new Vector3( currentLocation.x + dir.z, 0, currentLocation.z + dir.x ) );
             var moveCmd = new FTPCharacterMove( transform, dir, 1f, this );
             FTPGameManager.GetInstance().GetCommandManager().AddCommand( moveCmd );
         }
 
+        /// <summary>
+        ///     Check the target vector2 is walkable or not
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        private bool CanMoveTo( Vector3 dir )
+        {
+            var tempVec2 = new Vector3( currentLocation.x + dir.z, 0, currentLocation.z + dir.x );
+            var map = FTPGameManager.GetInstance().GetLevelManager().GetMap();
+            if ( ( int ) tempVec2.x >= 0 && ( int ) tempVec2.x < map.Count &&
+                 ( int ) tempVec2.z >= 0 && ( int ) tempVec2.z < map[ ( int ) tempVec2.x ].Count )
+                return map[ ( int ) tempVec2.x ][ ( int ) tempVec2.z ] == 0;
+            return false;
+        }
+
         #region Interfaces
 
+        /// <summary>
+        ///     Set the player can move or not
+        /// </summary>
+        /// <param name="target"></param>
         public void SetCanMove( bool target )
         {
             _canMove = target;
+        }
+
+        /// <summary>
+        ///     Set current location of the player
+        /// </summary>
+        /// <param name="location"></param>
+        public void SetCurrentLocation( Vector3 location )
+        {
+            currentLocation = location;
+            var idx = ( int ) location.x * FTPGameManager.GetInstance().GetLevelManager().GetMap().Count +
+                      ( int ) location.z;
+            var trans = FTPGameManager.GetInstance().GetLevelManager().GetRoadTransforms()[ idx ];
+            if ( trans != null && trans.TryGetComponent( out FTPRoad road ) )
+                road.ChangeColor( Color.green );
         }
 
         #endregion
--- Assets/Scripts/Games/FindThePatternsGame/Managers/FTPLevelManager.cs
+++ Assets/Scripts/Games/FindThePatternsGame/Managers/FTPLevelManager.cs
@@ -51,12 +51,23 @@ namespace Games.FindThePatternsGame.Managers
             // Find the entrance and instantiate the player
             var entrance =
                 _roadTransforms.Find( r => r.GetComponent< FTPRoad >().GetRoadType() == FTPRoadType.Entrance );
+
+            // Get the entrance location in vector 3
+            var entranceVec2 = Vector2.zero;
+            for ( var i = 0; i < _map.Count; i++ )
+            for ( var j = 0; j < _map[ i ].Count; j++ )
+                if ( _map[ i ][ j ] == 2 )
+                {
+                    entranceVec2 = new Vector3( i, 0, j );
+                    break;
+                }
+
             // Instantiate the player
             var player = Instantiate( playerTemplate, entrance.position + Vector3.up * 1, Quaternion.identity );
+            player.GetComponent< FTPPlayer >().SetCurrentLocation( entranceVec2 );
 
             // Move camera to the center of the map by getting transform from the road transforms
             var center = _roadTransforms[ initialMapSize / 2 * initialMapSize + initialMapSize / 2 ];
-            center.GetComponent< FTPRoad >().ChangeColor( Color.red );
             var cam = Camera.main;
             cam.transform.position = center.position + Vector3.up * 10f;
         }
@@ -89,6 +100,24 @@ namespace Games.FindThePatternsGame.Managers
         {
             level++;
             GenerateMap();
+        }
+
+        /// <summary>
+        ///     Get the map data
+        /// </summary>
+        /// <returns></returns>
+        public List< List< int > > GetMap()
+        {
+            return _map;
+        }
+
+        /// <summary>
+        ///     Get the road transforms
+        /// </summary>
+        /// <returns></returns>
+        public List< Transform > GetRoadTransforms()
+        {
+            return _roadTransforms;
         }
 
         #endregion
